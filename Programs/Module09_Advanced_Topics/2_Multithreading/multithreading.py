c34e7983f88242ff6dbf2a8b344763c85215b450
Logeswari Gunasekaran <123753061+logeswari-g@users.noreply.github.com>
2025-04-21 16:06:11 +0530

    Update multithreading.py

--- Programs/Module09_Advanced_Topics/2_Multithreading/multithreading.py
+++ Programs/Module09_Advanced_Topics/2_Multithreading/multithreading.py
@@ -1,43 +1,25 @@
 import threading
-import os
 import time
 
-def print_numbers(endnum):
-    for i in range(endnum):
-        print(i)
-
-def task1():
-    print("Task 1 assigned to thread: {}".format(threading.current_thread().name))
-    print("ID of process running task 1: {}".format(os.getpid()))
-    time.sleep(5)
-
-def task2():
-    print("Task 2 assigned to thread: {}".format(threading.current_thread().name))
-    print("ID of process running task 2: {}".format(os.getpid()))
+# Simulate sending email
+def send_email(user):
+    print(f"Sending email to {user}...")
+    time.sleep(2)  # Simulate delay (e.g., waiting for server)
+    print(f"Email sent to {user}")
 
 if __name__ == "__main__":
-    # Normal method
-    print('Executed by main thread')
-
-    # Create a thread
-    thread = threading.Thread(target=print_numbers, args=(1000,))
-
-    # Start the thread
-    thread.start()
-
-    # Wait for the thread to finish
-    thread.join()
-
-    print("ID of process running main program: {}".format(os.getpid()))
-    print("Main thread name: {}".format(threading.current_thread().name))
-
-    t1 = threading.Thread(target=task1, name='t1')
-    t2 = threading.Thread(target=task2, name='t2')
-
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
-
-    print("End of program")
+    # List of users
+    users = ['Alice', 'Bob', 'Charlie']
+    
+    # Create and start threads
+    threads = []
+    for user in users:
+        thread = threading.Thread(target=send_email, args=(user,))
+        thread.start()
+        threads.append(thread)
+    
+    # Wait for all threads to finish
+    for thread in threads:
+        thread.join()
+    
+    print("All emails sent.")
